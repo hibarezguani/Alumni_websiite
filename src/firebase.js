dd0e151396e84e0b8fdc23c3c1f35e8a6a022d0b
hibarezguani <rezganihiba12@gmail.com>
2024-01-14 12:50:54 +0100

    New changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,26 @@
-// Import the functions you need from the SDKs you need
+// firebase.js (ou tout autre fichier pertinent)
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import firebase from 'firebase/app';
 import 'firebase/auth';
 
+// Vérifiez si window est défini avant d'utiliser Firebase Analytics
+let analytics = null;
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+if (typeof window !== 'undefined') {
+  const firebaseConfig = {
+    apiKey: "AIzaSyA-yJP01U_KXJvYhd-9INpwKSlZo6YySjs",
+    authDomain: "alumni-47d55.firebaseapp.com",
+    projectId: "alumni-47d55",
+    storageBucket: "alumni-47d55.appspot.com",
+    messagingSenderId: "447074767374",
+    appId: "1:447074767374:web:233f1df0b67e90ea3ae25f",
+    measurementId: "G-4GG4BRHZHM"
+  };
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA-yJP01U_KXJvYhd-9INpwKSlZo6YySjs",
-  authDomain: "alumni-47d55.firebaseapp.com",
-  projectId: "alumni-47d55",
-  storageBucket: "alumni-47d55.appspot.com",
-  messagingSenderId: "447074767374",
-  appId: "1:447074767374:web:233f1df0b67e90ea3ae25f",
-  measurementId: "G-4GG4BRHZHM"
-};
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  analytics = getAnalytics(app);
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export default analytics;
